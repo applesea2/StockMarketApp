6696eaaac1d9d03c237bf026788cb3237addefa2
Jesse Stroster <stroste3@cse.msu.edu>
2025-04-13 22:42:21 -0400

    Added search as you type functionality to search box

--- StockMarketApp.UI/Program.cs
+++ StockMarketApp.UI/Program.cs
@@ -1,15 +1,25 @@
 using StockMarketApp.UI.Components;
 using System.Reflection.PortableExecutable;
+using StockMarketApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddHttpClient("StockMarketApp.Api",
-    client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("StockMarketApp.Api:BaseAddress")));
-
+builder.Services.AddHttpClient("ApiHttpClient", configureClient =>
+{
+    string? baseAddress = builder.Configuration.GetValue<string>("StockMarketApp.Api:BaseAddress");
+    try
+    {
+        configureClient.BaseAddress = new Uri(baseAddress);
+    }
+    catch (InvalidOperationException e)
+    {
+        throw new InvalidOperationException("Missing base url configuration");
+    }
+});
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
